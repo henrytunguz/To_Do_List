0cda52adc80a5fb9a0f97ef850b7f9a6f17066f1
henry <henrytunguz@gmail.com>
2025-01-20 13:47:43 -0800

    hi

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use chrono::{NaiveDate, NaiveTime};
+struct Task{
+    title: String,
+    details: String,
+    date:NaiveDate,
+    time:NaiveTime
+}
 fn main() {
     println!("Hello, world!");
 }
